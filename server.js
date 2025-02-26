201d7d08b359fc3ba7d00122018298188e748fd7
Sajin Saji George <sajinsajigeorge@gmail.com>
2025-02-26 11:06:26 +0530

    added

--- server.js
+++ server.js
@@ -14,32 +14,27 @@ app.post("/signup", (req, res) => {
     const { username, email, password, dob } = req.body;
 
     if (!username) {
-        return
-        res.status(400).json({ error: "Username cannot be empty" });
+        return res.status(400).json({ error: "Username cannot be empty" });
     }
     if (!email) {
-        return
-        res.status(400).json({ error: "Email cannot be empty" });
+        return res.status(400).json({ error: "Email cannot be empty" });
     }
-    if (!password || password.length < 8 || password.length >16) {
-        return
-        res.status(400).json({ error: "Password length should be greater than 8 and less than or equal to 16" });
+    if (!password || password.length < 8 || password.length > 16) {
+        return res.status(400).json({ error: "Password length should be greater than 8 and less than or equal to 16" });
     }
     if (!dob) {
-        return
-        res.status(400).json({ error: "Date of birth cannot be empty" });
+        return res.status(400).json({ error: "Date of birth cannot be empty" });
     }
 
     const existingUser = users.find(user => user.email === email);
     if (existingUser) {
-        return
-        res.status(400).json({ error: "Email is already registered" });
+        return res.status(400).json({ error: "Email is already registered" });
     }
 
     const newUser = { username, email, password, dob };
-    user.push(newUser);
+    users.push(newUser);
 
-    res.status(201).json({ message: "User signed up succesfully", user:newUser });
+    res.status(201).json({ message: "User signed up successfully", user: newUser });
 });
 
 app.listen(port, () => {
